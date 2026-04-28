ca79a02c00328a0fc2fc84cd2a089a67b529788f
sugarsoph <sophia.abgarian@gmail.com>
2026-04-28 19:53:43 +0100

    Update db.ts

--- api/_utils/db.ts
+++ api/_utils/db.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 export function supa(){
   const url = process.env.SUPABASE_URL!;
-  const key = process.env.SUPABASE_SERVICE_ROLE!;
+  const key = process.env.SUPABASE_ANON_KEY!;
   const client = createClient(url, key, { auth: { persistSession: false } });
   return client;
 }
