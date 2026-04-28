00ff10106d236f91b9fb1df3dc5bdb637d4654f3
sugarsoph <sophia.abgarian@gmail.com>
2026-04-28 20:16:44 +0100

    Update auth.ts

--- api/_utils/auth.ts
+++ api/_utils/auth.ts
@@ -1,15 +1,14 @@
 // api/_utils/auth.ts
-import type { VercelRequest, VercelResponse } from "@vercel/node";
 import cookie from "cookie";
 
-export function assertAuth(req: VercelRequest, res: VercelResponse){
+export function assertAuth(req: any, res: any){
   const cookies = cookie.parse(req.headers.cookie || "");
   if (cookies.session === "ok") return;
   res.status(401).json({ error: "Unauthorized" });
   throw new Error("halt");
 }
 
-export function setSessionCookie(req: VercelRequest, res: VercelResponse){
+export function setSessionCookie(req: any, res: any){
   const isLocal = (req.headers.host || "").includes("localhost") || process.env.NODE_ENV === "development";
   const serialized = cookie.serialize("session", "ok", {
     httpOnly: true,
